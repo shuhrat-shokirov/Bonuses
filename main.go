7bd8598254ac395cd0d9c696fbcb69425756afe4
Shuhrat.Shokirov <avatar_9999@list.ru>
2020-01-05 14:41:07 +0300

    replacement of variables

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ func main() {
 func Bonus(salary []int) int {
 	generalBonuses :=0
 	const MinBonuses  = 10_000
+	const perсent = 5
+	const MaxPercent = 100
 	for _, value := range salary{
 		if value >= MinBonuses {
-			generalBonuses += (value - MinBonuses) * 5 / 100
+			generalBonuses += (value - MinBonuses) * perсent / MaxPercent
 		}
 	}
 	return generalBonuses
