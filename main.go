b90f02eff592320072492b3bc74994c8db414a40
Shuhrat.Shokirov <avatar_9999@list.ru>
2020-01-07 16:36:30 +0300

    correction of variables add tests

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 func main() {
 }
-func Bonus(salary []int) int {
+func Bonus(sale []int) int {
 	generalBonuses :=0
-	const MinBonuses  = 10_000
+	const minbonuses  = 10_000
 	const perсent = 5
-	const MaxPercent = 100
-	for _, value := range salary{
-		if value >= MinBonuses {
-			generalBonuses += (value - MinBonuses) * perсent / MaxPercent
+	for _, value := range sale{
+		if value >= minbonuses{
+			generalBonuses += (value - minbonuses) * perсent / 100
 		}
 	}
 	return generalBonuses
--- main_test.go
+++ main_test.go
@@ -5,16 +5,17 @@ import "testing"
 func TestBonus(t *testing.T) {
 	testCases := []struct {
 		name string
-		salery []int
+		sale []int
 		want int
 	}{
-		{"First Employees", []int{12_000, 8_000, 15_000, 8_000},350},
-		{"First Employees", []int{10_000, 8_000, 10_000, 8_000},0},
-		{"First Employees", []int{9_000, 8_000, 9_000, 8_000},0},
+		{"All employees received a bonus", []int{12_000, 18_000, 15_000, 13_000},900},
+		{"A couple of employees received a bonus", []int{12_000, 10_000, 15_000, 8_000},350},
+		{"Employees did not deserve bonuses", []int{7_000, 8_000, 9_000, 9_500},0},
+		{"All employees deserve a bonus do not receive", []int{10_000, 10_000, 10_000, 10_000},0},
 		// TODO: Add test cases.
 	}
 	for _, testCase := range testCases {
-		got := Bonus(testCase.salery)
+		got := Bonus(testCase.sale)
 		if got != testCase.want {
 			t.Error("nps with test:", testCase.name, "got:", got, "want:", testCase.want)
 		}
